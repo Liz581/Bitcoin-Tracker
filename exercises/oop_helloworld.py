e2c6b1949f74d8fd269eb7f6c1908238123eb4b4
Liz581 <88364712+Liz581@users.noreply.github.com>
2022-05-09 13:11:42 -0500

    Car class

--- exercises/oop_helloworld.py
+++ exercises/oop_helloworld.py
@@ -1,4 +1,14 @@
 # create your class here
+class Car:
+    def __init__(self, model, speed):
+        self.model = model
+        self.speed = speed
+
+    def getModel(self):
+        return self.model
+
+    def setSpeed(self, value):
+        self.speed = value
 
 if __name__ == '__main__':
     bmw = Car('BMW', 30.3)
